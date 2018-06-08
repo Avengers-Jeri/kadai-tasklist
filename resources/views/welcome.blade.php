ec6878fe631a6b4d731b9e9cc37b4dff4a20d113
Avengers-Jeri <kt274.ku58.st@gmail.com>
2018-06-08 04:22:04 +0000

    chapter9 complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-<!--
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
@@ -93,20 +92,4 @@
     </body>
 </html>
 
--->
 
-@extends('layouts.app')
-
-@section('content')
-    @if (Auth::check())
-        <?php $user = Auth::user(); ?>
-        {{ $user->name }}
-    @else
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to Tasklist</h1>
-                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
-            </div>
-        </div>
-    @endif
-@endsection
