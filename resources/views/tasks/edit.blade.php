e9fff7e5accffaeb1dc8ea62af72a5b42928824e
Avengers-Jeri <kt274.ku58.st@gmail.com>
2018-06-11 01:28:02 +0000

    re-submitting

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
- 
+   @if (Auth::check())
+   
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
     <div class="row">
@@ -23,6 +24,13 @@
         </div>
     </div>
 
-
+     @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to Tasklist</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
+    @endif
 
 @endsection
